673c34f4117fa5f5c34ed095e1751c22cdbe7d48
Chris Hambacher <hambcastlesen12@gmail.com>
2020-04-14 09:53:18 -0700

    Continued work on DES
    Added permutations, main thing that's needed is the Feistel network.

--- encryption/des.js
+++ encryption/des.js
@@ -1,56 +1,26 @@
-class DES {
-
-    constructor() {}
-
-    // Key is 8 bytes (64 bits).
-    // Block size is 8 bytes (64 bits).
-    encrypt(plaintext, key) {
-        
-        // Pad the plaintext (using spaces as padding).
-        while(plaintext.length % 8 != 0) {
-            plaintext += " ";
-        }
+let DES_Subkeys = {
     
-        plaintext = convertToBits(plaintext);
-    
-        return plaintext;
-    
-    }
-    
-    decrypt(encryptedtext, key) {
-    
-    }
+    // Lookup tables.
+    _PC1: 
+    [[57,49,41,33,25,17,9,1],
+    [58,50,42,34,26,18,10,2],
+    [59,51,43,35,27,19,11,3],
+    [60,52,44,36,63,55,47,39],
+    [31,23,15,7,62,54,46,38],
+    [30,22,14,6,61,53,45,37],
+    [29,21,13,5,28,20,12,4]],
 
+    _PC2: 
+    [[14,17,11,24,1,5,3,28],
+    [15,6,21,10,23,19,12,4],
+    [26,8,16,7,27,20,13,2],
+    [41,52,31,37,47,55,30,40],
+    [51,45,33,48,44,49,39,56],
+    [34,53,46,42,50,36,29,32]],   
 
-
-}
-
-class DES_Subkeys {
-
-    constructor(key) {
-        this._key = key;
-
-        // Lookup tables.
-        this._PC1 = 
-        [[57,49,41,33,25,17,9,1],
-        [58,50,42,34,26,18,10,2],
-        [59,51,43,35,27,19,11,3],
-        [60,52,44,36,63,55,47,39],
-        [31,23,15,7,62,54,46,38],
-        [30,22,14,6,61,53,45,37],
-        [29,21,13,5,28,20,12,4]]; 
-        this._PC2 = 
-        [[14,17,11,24,1,5,3,28],
-        [15,6,21,10,23,19,12,4],
-        [26,8,16,7,27,20,13,2],
-        [41,52,31,37,47,55,30,40],
-        [51,45,33,48,44,49,39,56],
-        [34,53,46,42,50,36,29,32]];   
-    }
-
-    generateSubkeysEncryption() {
+    generateSubkeysEncryption: function(key) {
         let subkeys = [];
-        let firstOutput = pc1(this.key);
+        let firstOutput = pc1(key);
         let c = firstOutput.substring(0, 28);
         let d = firstOutput.substring(28);
 
@@ -69,9 +39,9 @@ class DES_Subkeys {
         }
         return subkeys;
 
-    }
+    },
 
-    generateSubkeysDecryptions() {
+    generateSubkeysDecryptions: function(key) {
         let subkeys = [];
         let firstOutput = pc1(key);
         let c = firstOutput.substring(0, 28);
@@ -93,9 +63,9 @@ class DES_Subkeys {
             subkeys[round-1] = pc_2(c + d);
         }
         return subkeys;
-    }
+    },
 
-    pc1() {
+    pc1: function(key) {
         let result = "";
         for(let row = 0; row < 7; row++){
             for(let col = 0; col < 8; col++){
@@ -103,8 +73,8 @@ class DES_Subkeys {
             }
         }
         return result;
-    }
-    pc2() {
+    },
+    pc2: function(key) {
         let result = "";
         for(let row = 0; row < 6; row++){
             for(let col = 0; col < 8; col++){
@@ -114,5 +84,86 @@ class DES_Subkeys {
         return result;
     }
 }
+let Permutation = {
+    _IPTable: 
+    [[58,50,42,34,26,18,10,2],
+    [60,52,44,36,28,20,12,4],
+    [62,54,46,38,30,22,14,6],
+    [64,56,48,40,32,24,16,8],
+    [57,49,41,33,25,17, 9,1],
+    [59,51,43,35,27,19,11,3],
+    [61,53,45,37,29,21,13,5],
+    [63,55,47,39,31,23,15,7]],
+
+    _IPInverseTable:
+    [[40,8,48,16,56,24,64,32],
+    [39,7,47,15,55,23,63,31],
+    [38,6,46,14,54,22,62,30],
+    [37,5,45,13,53,21,61,29],
+    [36,4,44,12,52,20,60,28],
+    [35,3,43,11,51,19,59,27],
+    [34,2,42,10,50,18,58,26],
+    [33,1,41, 9,49,17,57,25]],
+
+    initialPermute: function(bits) {
+        let result = "";
+        for(let row=0; row<8; row++){
+           for(let col =0; col<8; col++){
+              result += bits.charAt(this._IPTable[row][col]-1);
+            }
+        }
+        return result;
+    },
+
+    finalPermute: function() {
+        let result = "";
+        for(let row=0; row<8; row++){
+           for(let col =0; col<8; col++){
+              result += bits.charAt(this._IPInverseTable[row][col]-1);
+            }
+        }
+        return result;
+    }
+}
+
+class DES {
+
+    constructor() {
+
+    }
+
+    // Key is 8 bytes (64 bits).
+    // Block size is 8 bytes (64 bits).
+    encrypt(plaintext, key) {
+        
+        // Pad the plaintext (using spaces as padding).
+        while(plaintext.length % 8 != 0) {
+            plaintext += " ";
+        }
+        plaintext = convertToBits(plaintext);
+        
+        // Encrypt the plaintext (64 bits at a time).
+        for(let i = 0; i < plaintext.length / 64; i++) {
+            let blockOfPlaintext = plaintext.slice(i * 64, (i + 1) * 64);
+            let subkeys = DES_Subkeys.generateSubkeysEncryption(key);
+            Permutation.initialPermute(blockOfPlaintext);
+        }
+
+
+
+
+        return plaintext;
+    
+    }
+    
+    decrypt(encryptedtext, key) {
+    
+    }
+
+
+
+}
+
+
 
 
